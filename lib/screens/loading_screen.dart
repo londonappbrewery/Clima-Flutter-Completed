3aebdbf4a2ab82cc30c275060cee112476ae12e5
Flavian Anselmo <68228894+flavian-anselmo@users.noreply.github.com>
2021-04-30 11:58:37 +0300

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,39 +1,66 @@
 import 'package:flutter/material.dart';
-import 'location_screen.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:clima/services/weather.dart';
+import 'package:clima/utilities/constants.dart';
 
-class LoadingScreen extends StatefulWidget {
+class CityScreen extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _LoadingScreenState();
-  }
+  _CityScreenState createState() => _CityScreenState();
 }
 
-class _LoadingScreenState extends State<LoadingScreen> {
-  @override
-  void initState() {
-    super.initState();
-    getLocationData();
-  }
-
-  void getLocationData() async {
-    var weatherData = await WeatherModel().getLocationWeather();
-
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen(
-        locationWeather: weatherData,
-      );
-    }));
-  }
-
+class _CityScreenState extends State<CityScreen> {
+  String cityTownName;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SpinKitDoubleBounce(
-          color: Colors.white,
-          size: 100.0,
+      body: Container(
+        decoration: BoxDecoration(
+          //to have a background screen
+          image: DecorationImage(
+            image: AssetImage('images/city_background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topLeft,
+                // ignore: deprecated_member_use
+                child: FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    size: 50.0,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                child: TextField(
+                  style: TextStyle(color: Colors.blueGrey, fontSize: 20.0),
+                  decoration: ktextfielddecoration,
+                  onChanged: (value) {
+                    //fetch the user input
+                    cityTownName = value;
+                  },
+                ),
+              ),
+
+              // ignore: deprecated_member_use
+              FlatButton(
+                color: Colors.blueGrey,
+                onPressed: () {
+                  Navigator.pop(context, cityTownName);
+                },
+                child: Text(
+                  'Get Weather',
+                  style: kButtonTextStyle,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
