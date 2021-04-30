d58124183a1eb5691813bf6a3e3e5681ac7e7cd7
Flavian Anselmo <68228894+flavian-anselmo@users.noreply.github.com>
2021-04-30 11:59:46 +0300

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,66 +1,49 @@
-import 'package:flutter/material.dart';
-import 'package:clima/utilities/constants.dart';
+//import 'dart:convert';
 
-class CityScreen extends StatefulWidget {
+import 'package:clima/screens/location_screen.dart';
+import 'package:clima/services/location.dart';
+import 'package:clima/services/weather.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+Location loc = new Location();
+
+class LoadingScreen extends StatefulWidget {
   @override
-  _CityScreenState createState() => _CityScreenState();
+  _LoadingScreenState createState() => _LoadingScreenState();
 }
 
-class _CityScreenState extends State<CityScreen> {
-  String cityTownName;
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    //get the location asynchronously
+    get_currentLocation();
+  }
+
+  // ignore: non_constant_identifier_names
+  Future<void> get_currentLocation() async {
+    WeatherModel weatherModel = new WeatherModel();
+    //get weather data
+    var weatherdata = await weatherModel.get_currentLocation();
+    //PUSH THE WEATHER DATA TO THE NEXT SCREEN
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationweather: weatherdata,
+      );
+    }));
+  }
+
+  //fetch data form the inernet
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          //to have a background screen
-          image: DecorationImage(
-            image: AssetImage('images/city_background.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        constraints: BoxConstraints.expand(),
-        child: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                // ignore: deprecated_member_use
-                child: FlatButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Icon(
-                    Icons.arrow_back_ios,
-                    size: 50.0,
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: TextField(
-                  style: TextStyle(color: Colors.blueGrey, fontSize: 20.0),
-                  decoration: ktextfielddecoration,
-                  onChanged: (value) {
-                    //fetch the user input
-                    cityTownName = value;
-                  },
-                ),
-              ),
-
-              // ignore: deprecated_member_use
-              FlatButton(
-                color: Colors.blueGrey,
-                onPressed: () {
-                  Navigator.pop(context, cityTownName);
-                },
-                child: Text(
-                  'Get Weather',
-                  style: kButtonTextStyle,
-                ),
-              ),
-            ],
-          ),
+      body: Center(
+        child: SpinKitRipple(
+          color: Colors.white,
+          size: 100.0,
         ),
       ),
     );
